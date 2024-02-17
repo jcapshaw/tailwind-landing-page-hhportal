8d1d5a89860ca224c466f67bbb0f51fe089413a6
Jeff Capshaw <51003293+jcapshaw@users.noreply.github.com>
2024-02-17 02:49:11 +0000

    feat: Update transition rules, add dollar sign in the dollar-container class and fix typo  file

--- components/features.tsx
+++ components/features.tsx
@@ -94,11 +94,11 @@ export default function Features() {
                 >
                   <div>
                     <div className="font-bold leading-snug tracking-tight mb-1">
-                      Building the Simple ecosystem
+                      Lookup Inventory and Send a Quote
                     </div>
                     <div className="text-gray-600">
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
+                      Real-time proposal estimates that you can share instantly
+                      with your customers.
                     </div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
@@ -131,17 +131,7 @@ export default function Features() {
                     </div>
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
-                    <svg
-                      className="w-3 h-3 fill-current"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z"
-                        fill="#191919"
-                        fillRule="nonzero"
-                      />
-                    </svg>
+                    $
                   </div>
                 </a>
               </div>
